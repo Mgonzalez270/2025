a2ceb9e7cd4d6d40b908263dbc5d100381c0cc52
Mariuxi Gonzalez <mari.gonz407@gmail.com>
2025-01-19 14:59:33 -0500

    project message

---
+++ e.1/3 messages/src/message.java
@@ -0,0 +1,7 @@
+public class message {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Computer Science!");
+        System.out.println("Programming is fun");
+    }
+}
