04a293e284499a24eac43073d8a76c2f7b580315
Mariuxi Gonzalez <mari.gonz407@gmail.com>
2025-01-19 15:27:39 -0500

    project loop

---
+++ e1.2/repeat/README.md
@@ -0,0 +1,12 @@
+## Pseudocode
+
+BEGIN
+    SET message TO "Welcome to Java"
+    SET repeat TO 5
+    SET i TO 0
+
+    WHILE i < repeat DO
+        PRINT message
+        INCREMENT i BY 1
+    END WHILE
+END
---
+++ e1.2/repeat/src/loop.java
@@ -0,0 +1,12 @@
+public class loop {
+    public static void main(String[] args) {
+        String message = "Welcome to Java";
+        int repeat = 5;
+        int i = 0;
+
+        while (i < repeat) {
+            System.out.println(message);
+            i++;
+        }
+    }
+}
