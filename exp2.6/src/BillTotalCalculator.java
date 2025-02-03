3c71534d5599fd66a852eb30e0200db5fdeadd6f
Mariuxi Gonzalez <mari.gonz407@gmail.com>
2025-02-02 22:05:30 -0500

    Bill update

--- exp2.6/src/BillTotalCalculator.java
+++ exp2.6/src/BillTotalCalculator.java
@@ -6,17 +6,17 @@ public class BillTotalCalculator {
         try (
                 Scanner scanner = new Scanner(System.in)) {
             while (true) {
-                System.out.print(s:"Enter subtotal: ");
+                System.out.print("Enter subtotal: ");
                 double subtotal = scanner.nextDouble();
-                System.out.println(s:"Enter gratuity rate (as a percentage): ");
+                System.out.print("Enter gratuity rate (as a percentage): ");
                 double gratuityRate = scanner.nextDouble();
                 double gratuity = subtotal * (gratuityRate / 100);
-                double total = subtotal + gratuity
+                double total = subtotal + gratuity;
                 System.out.println("Gratuity: " + gratuity);
                 System.out.println("Total: " + total);
-                System.out.print(s: "Do you want to perform another calculation? (y/n): ");
+                System.out.print("Do you want to perform another calculation? (y/n): ");
                 String response = scanner.next();
-                if (response.equalsIgnoreCase(anotherString:"n")) {
+                if (response.equalsIgnoreCase("n")) {
                     break;
                 }
             }
